7a122dfe74332f3a95bae35f462cae2202887713
sls1005 <90055573+sls1005@users.noreply.github.com>
2025-06-18 12:30:02 +0800

    Bug fix.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "test.sls1005.projects.codeevaluatorkeyboard"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
     }
     signingConfigs {
         register("release") {
